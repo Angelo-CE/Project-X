523eb4383092b197957371964c3703c07a2f02eb
FABIAN_CE <fabianceciliano@estudiantec.cr>
2021-08-25 23:54:30 -0600

    Update Clases

--- src/Server/Client.java
+++ src/Server/Client.java
@@ -1,4 +1,15 @@
 package Server;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Client {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Username:   and Port:  ");
+        String[] setupValues = bReader.readLine().split(" ");
+        ServerThread sThread = new ServerThread(setupValues[1]);
+
+    }
 }
