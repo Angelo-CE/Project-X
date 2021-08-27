56589470e9855ed701613bd24392597cccae4571
FABIAN_CE <fabianceciliano@estudiantec.cr>
2021-08-26 23:20:37 -0600

    Cambio de metodo

--- src/Server/server.java
+++ src/Server/server.java
@@ -1,15 +1,17 @@
+/*
 package Server;
 
 import javax.xml.crypto.Data;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class server {
-    public static void main(String[] args) {
-        try {
+   // public static void main(String[] args) {
+       try {
             ServerSocket ss = new ServerSocket(8000);
             System.out.println("Wait for connection : ");
             Socket s = ss.accept();
@@ -30,8 +32,9 @@ public class server {
                     System.exit(0);
                 }
             }
-
-
-        }
+        } catch (IOException e) {
+           e.printStackTrace();
+       }
     }
 }
+*/
